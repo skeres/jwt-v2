ffc555e7dcb267533baed581f092c4d5999e17da
skeres <stephane.keresztes@gmail.com>
2021-08-30 19:18:13 +0200

    initial

---
+++ src/test/java/com/sks/jwtv2/JwtV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sks.jwtv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
